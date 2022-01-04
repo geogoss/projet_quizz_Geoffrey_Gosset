0e1f365772ddf5993b846dc572bb4fd78f1c8ed3
geoffrey <geoffreygosset123@gmail.com>
2022-01-04 15:41:12 +0100

    switch ok il reste la fonction couleur

--- 1. Quizz/app.js
+++ 1. Quizz/app.js
@@ -10,20 +10,22 @@ const aideResultat = document.querySelector(".aide");
 const toutesLesQuestions = document.querySelectorAll(".questionBlock");
 let verifTableau = [];
 
-
+// créer un événement sur le bouton, pour stocker les informations du formulaire
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
+    e.preventDefault(); //pour enlever les paramètres par défault qui envoie sur une autre page
     // console.log(document.querySelector('input[name="q1"]:checked').value);
-
+    // boucle pour mettre les valeurs de chaque réponse dans un tableau déjà créé
     for(i = 1; i < 6; i++) {
         tableauResultats.push(document.querySelector(`input[name="q${i}"]:checked`).value)
     }
     console.log(tableauResultats);
     verification(tableauResultats);
-    tableauResultats = [];
+    tableauResultats = []; //vider tableau après chaque utilisation
 })
 
-let verification = function (x) {
+
+    // fonction qu'on créé pour comparer un tableau résultats avec un tableau bonnes réponses
+    let verification = function (x) {
 
     for(let a = 0; a < 5; a++){
 
@@ -35,5 +37,59 @@ let verification = function (x) {
 
     }
     console.log(verifTableau);
+    afficherResultats(verifTableau);
+    verifTableau = []; //vider tableau après chaque utilisation
 }
+
+// fonction pour compter le nombre de false (et de true) dans le tableau
+function afficherResultats(tabCheck) {
+
+    const nbDeFautes = tabCheck.filter(el => el !== true).length;
+    // console.log(nbDeFautes);
+
+    switch(nbDeFautes) {
+
+        case 0:
+            titreResultat.innerText = `✔️ Bravo, c'est un sans faute ! ✔️`
+            aideResultat.innerText = ''
+            noteResultat.innerText = '5/5'
+            break;
+        case 1:
+            titreResultat.innerText = `✨ Vous y êtes presque ! ✨`
+            aideResultat.innerText = 'Retentez une autre réponse dans la case rouge, puis re-validez !'
+            noteResultat.innerText = '4/5'
+            break;
+        case 2:
+            titreResultat.innerText = `✨ Encore un effort ... 👀`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '3/5'
+            break;
+        case 3:
+            titreResultat.innerText = `👀 Il reste quelques erreurs. 😭`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '2/5'
+            break;
+        case 4:
+            titreResultat.innerText = `😭 Peux mieux faire ! 😭`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '1/5'
+            break;
+        case 5:
+            titreResultat.innerText = `👎 Peux mieux faire ! 👎`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '0/5'
+        break;
+
+        default:
+            'Wops, cas innatendu.';
+
+    }
+
+}
+
+
+
+
+
+
 
